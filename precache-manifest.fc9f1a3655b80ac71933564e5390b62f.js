48f30b5a47245887472b23cb16b2948912f83d60
Stephanie Zhu <slzzhu@gmail.com>
2020-10-26 22:24:51 -0700

    Updates

---
+++ precache-manifest.fc9f1a3655b80ac71933564e5390b62f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14da0fd8312545bc894dd3b85fd908b6",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "c77099919e209a00d5a8",
+    "url": "/recipes/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "e1677d766a31104be00c",
+    "url": "/recipes/static/css/main.2731de43.chunk.css"
+  },
+  {
+    "revision": "c77099919e209a00d5a8",
+    "url": "/recipes/static/js/2.4df8eae2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/recipes/static/js/2.4df8eae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1677d766a31104be00c",
+    "url": "/recipes/static/js/main.db018b73.chunk.js"
+  },
+  {
+    "revision": "65aed4a3aaa500d440aa",
+    "url": "/recipes/static/js/runtime-main.9fddb04f.js"
+  }
+]);
